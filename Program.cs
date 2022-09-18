d43a6858f9b3b245e55da440f9eae20cf6dcd121
Joseph Sollestre <106940326+aLumie@users.noreply.github.com>
2022-09-18 15:07:44 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Activity3
+namespace Struct
 {
-    internal class Program
+    public class StructAct
     {
-        static void Main(string[] args)
+        struct Employee
         {
-            
-            string[] email = { "email1@gmail.com", "email2@gmail.com", "email@gmail.com" };
-            string[] username = { "username1", "username2", "username3" };
-            string[] password = { "password1", "password2", "password3" };
-
-            //Input the index value
-            Console.WriteLine("Index: ");
-            int index = Convert.ToInt32(Console.ReadLine());
-
-            // Displaying the values
-            Console.WriteLine("Email: " + email[index]);
-            Console.WriteLine("Username: " + username[index]); 
-            Console.WriteLine("Password: " + password[index]);
-
+            public string firstName;
+            public string lastName;
+            public int age;
+            public string completedTraining;
 
         }
-        
 
-     
+        public static void Main(string[] args)
+        {
+            {
+                Employee employee1;
+
+                Console.WriteLine("Enter your First Name: ");
+                employee1.firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter your Last Name: ");
+                employee1.lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter your Age: ");
+                employee1.age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Do you have a completed training?");
+                employee1.completedTraining = Console.ReadLine();
+
+                Console.WriteLine("\nEmployee's Information");
+                Console.WriteLine("Name : " + employee1.firstName + " " + employee1.lastName);
+                Console.WriteLine("Age : " + employee1.age);
+                Console.WriteLine("Completed Training? " + employee1.completedTraining);
+
+
+            }
+
+        }
     }
 }
+
+    
