76925210fe9bf4683d21ef177fb3fda3abe6ebda
Joseph Sollestre <106940326+aLumie@users.noreply.github.com>
2022-09-18 15:06:40 +0800

    Add files via upload

---
+++ Act3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Activity3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] email = { "email1@gmail.com", "email2@gmail.com", "email@gmail.com" };
+            string[] username = { "username1", "username2", "username3" };
+            string[] password = { "password1", "password2", "password3" };
+
+            //Input the index value
+            Console.WriteLine("Index: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            // Displaying the values
+            Console.WriteLine("Email: " + email[index]);
+            Console.WriteLine("Username: " + username[index]); 
+            Console.WriteLine("Password: " + password[index]);
+
+
+        }
+        
+
+     
+    }
+}
---
+++ Act4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Activity4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                Console.WriteLine("==ARRAY PROGRAM==");
+                const int n = 5;
+                int[] numbers = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Enter Element: ", i + 1);
+                    string str = Console.ReadLine();
+                    numbers[i] = int.Parse(str);
+                }
+
+                Console.Write("\nOdd numbers: ");
+                for (int i = 0; i < n; i++)
+                {
+                    if (numbers[i] % 2 == 1)
+                    Console.Write("{0} ", numbers[i]);
+                }
+                Console.WriteLine();
+
+                Console.Write("Even numbers: ");
+                for (int i = 0; i < n; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    Console.Write("{0} ", numbers[i]);
+                }
+                Console.WriteLine();
+           
+        }
+    }
+}
